1a194bb07b0818cda0463fdb43b49448454ba79f
Thanakorn Kriangudom <thanakorn.vsalab@gmail.com>
2020-05-10 16:00:27 +0700

    frist_project

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Arduino-MicroWifi
-System Arduino Login Wifi.
+
+System Arduino Login Wifi & MQTT messages to prae.
+
+> Hardware system requirement: [Node MCU](https://en.wikipedia.org/wiki/NodeMCU)
+
+### Step Install Software Basic:
+1. Instarll Arduino IDE By [Download Tools](https://www.arduino.cc/en/Main/Software)
+![Step1](https://i.imgur.com/Hu2mGe1.png)
+
+2. When Install Arduino IDE success & open program Arduino IDE.
+![Step2](https://i.imgur.com/YYMRgwD.png)
+
+3. Go to Menu File >> Preferences >> Add URL >> Addition Board Manager URLs : http://arduino.esp8266.com/stable/package_esp8266com_index.json
+![Step3](https://i.imgur.com/BiQKg5M.png)
+
+4. Then OK, and Go to Menu Tools >> Board:”xxxxxx” >> Board Manager…
+![Step4](https://i.imgur.com/SoqdNBm.png)
+
+5. Type is Contributed. Find and select ESP8266 and Cilck Install.
+![Step5](https://i.imgur.com/oJRtwOE.png)
+
+6. Finished, you can select Menu Tools >> Board: NodeMCU1.0”
+![Step6](https://i.imgur.com/EuBfAoa.png)
+
+## Step Install Module Basic:
+---
+1. Go to Menu Tools  >> Library Manager ::
+![Step8](https://i.imgur.com/UXUe3Mb.png)
+
+## Library
+- [x] [WiFiManager](https://github.com/tzapu/WiFiManager) Author: tzapu
+
+- [x] [PubSubClient](https://github.com/knolleary/pubsubclient) Author: Nick O'Leary
+
+- [x] [ArduinoJson](https://github.com/bblanchon/ArduinoJson)
+Author: Benoit Blanchon
+
+## Deployment System
+---
+2. Deploy File code :: [Smart-wifi.ino]() on Node-MCU.
---
+++ Smart-wifi.ino
@@ -0,0 +1,201 @@
+//library
+#include <ESP8266WiFi.h>         
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         
+#include <ArduinoJson.h>     
+#include <PubSubClient.h>
+
+// delay
+#define D0 16   
+#define LED D0  
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+#define mqtt_server       "www.km1.io"
+#define mqtt_port         "1883"
+#define mqtt_user         "toon"
+#define mqtt_pass         "10205"
+#define mqtt_topic        "/toon/room5"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+  //Swicts
+  pinMode(LED,OUTPUT);
+  //clean FS for testing 
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_user, json["mqtt_user"]);
+          strcpy(mqtt_pass, json["mqtt_pass"]);
+          strcpy(mqtt_topic, json["mqtt_topic"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_user("user", "mqtt user", mqtt_user, 20);
+  WiFiManagerParameter custom_mqtt_pass("pass", "mqtt pass", mqtt_pass, 20);
+  WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 40);
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  // Reset Wifi settings for testing  
+  wifiManager.resetSettings();
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_user);
+  wifiManager.addParameter(&custom_mqtt_pass);
+  wifiManager.addParameter(&custom_mqtt_topic);
+  //reset settings - for testing
+  wifiManager.resetSettings();
+
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("Smart_PlugIN", "vsalab2018")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_user, custom_mqtt_user.getValue());
+  strcpy(mqtt_pass, custom_mqtt_pass.getValue());
+  strcpy(mqtt_topic, custom_mqtt_topic.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_user"] = mqtt_user;
+    json["mqtt_pass"] = mqtt_pass;
+    json["mqtt_topic"] = mqtt_topic;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+  String inString = "";  
+  inString += mqtt_port;
+  const uint16_t mqtt_port_x = inString.toInt(); 
+  client.setServer(mqtt_server, mqtt_port_x);
+  client.setCallback(callback);
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ String msg = "";
+ int i=0;
+ while (i<length) msg += (char)payload[i++];
+ if (msg == "on" or msg == "ON" or msg == "1") {
+   client.publish(mqtt_topic,"Switch-ON"); 
+   digitalWrite(LED,HIGH);
+   return;
+ }
+ else if(msg == "off" or msg == "OFF" or msg == "0"){
+   client.publish(mqtt_topic,"Switch-OFF"); 
+   digitalWrite(LED,LOW);
+   return;
+ }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+}
